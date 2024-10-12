f2a01c70fe635aeff0272bbc9f7b2d4d38bf7e8e
Ángel Díaz <ad9311@proton.me>
2024-10-12 15:38:33 -0500

    Implement load .env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/ad9311/renio-go/internal/db"
 	"github.com/ad9311/renio-go/internal/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Print("RENIO\n\n")
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	db.Init()
 
-	err := http.ListenAndServe(":8080", routes.Router())
+	err = http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), routes.Router())
 	if err != nil {
 		fmt.Printf("there's been an error: %s", err.Error())
 	}
--- internal/db/db.go
+++ internal/db/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"sync"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -19,10 +20,8 @@ func Init() {
 	once.Do(func() {
 		fmt.Println("! Connecting to database...")
 
-		dbURL := "postgres://ad9311:@localhost:5432/renio_go_development"
-
 		var err error
-		pool, err = pgxpool.New(context.Background(), dbURL)
+		pool, err = pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 		if err != nil {
 			log.Fatalf("x Unable to connect to database: %v\n", err)
 		}
