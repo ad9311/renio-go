3d038f35d8054514cbcd1c93144975ead66363db
Ángel Díaz <ad9311@proton.me>
2024-10-13 08:02:44 -0500

    Improve create jwt token on signing

--- internal/lib/jwt.go
+++ internal/lib/jwt.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_KEY"))
@@ -12,8 +13,11 @@ var jwtSecret = []byte(os.Getenv("JWT_KEY"))
 func CreateJWTToken(username string) (string, error) {
 	claims := jwt.MapClaims{
 		"sub": username,
-		"exp": time.Now().Add(time.Hour * 24 * 7).Unix(), // Set expiration for 24 hours
-		"iat": time.Now().Unix(),                         // Issued at
+		"aud": "https://renio.dev",
+		"iss": "https://api.renio.dev",
+		"exp": time.Now().Add(time.Hour * 24 * 7).Unix(),
+		"iat": time.Now().Unix(),
+		"jti": uuid.New().String(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
