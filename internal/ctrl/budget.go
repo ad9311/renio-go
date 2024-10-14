5f335ce9084afcce9010cbc5dd3ede47d27f5ee1
Ángel Díaz <ad9311@proton.me>
2024-10-14 09:23:19 -0500

    Implement index budgets

--- internal/ctrl/budget.go
+++ internal/ctrl/budget.go
@@ -10,11 +10,49 @@ import (
 
 func BudgetRouter(r chi.Router) func(r chi.Router) {
 	return func(r chi.Router) {
+		r.Get("/", indexBudgets)
+		r.Get("/current", findCurrentBudget)
 		r.Post("/", createBudget)
 	}
 }
 
+// actions //
+
+func indexBudgets(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value(conf.UserIDContext).(int)
+	var budgetAccount model.BudgetAccount
+	if err := budgetAccount.FindByUserID(userID); err != nil {
+		WriteError(w, []string{"no budget account found"}, http.StatusInternalServerError)
+		return
+	}
+
+	var budgets model.Budgets
+	if err := budgets.Index(budgetAccount.ID); err != nil {
+		WriteError(w, []string{err.Error()}, http.StatusNotFound)
+		return
+	}
+
+	WriteOK(w, budgets, http.StatusOK)
+}
+
 func createBudget(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value(conf.UserIDContext).(int)
+	var budgetAccount model.BudgetAccount
+	if err := budgetAccount.FindByUserID(userID); err != nil {
+		WriteError(w, []string{"no budget account found"}, http.StatusInternalServerError)
+		return
+	}
+
+	var budget model.Budget
+	if err := budget.Create(budgetAccount.ID); err != nil {
+		WriteError(w, []string{err.Error()}, http.StatusNotFound)
+		return
+	}
+
+	WriteOK(w, budget, http.StatusCreated)
+}
+
+func findCurrentBudget(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value(conf.UserIDContext).(int)
 	var budgetAccount model.BudgetAccount
 	if err := budgetAccount.FindByUserID(userID); err != nil {
--- internal/model/budget.go
+++ internal/model/budget.go
@@ -8,8 +8,6 @@ import (
 	"github.com/ad9311/renio-go/internal/db"
 )
 
-// Query functions //
-
 type Budget struct {
 	ID               int     `json:"id"`
 	UID              string  `json:"uid"`
@@ -19,6 +17,48 @@ type Budget struct {
 	TransactionCount int     `json:"transactionCount"`
 	IncomeCount      int     `json:"incomeCount"`
 	ExpenseCount     int     `json:"expenseCount"`
+}
+
+type Budgets []Budget
+
+const budgetSelectedColumns = `id, uid, balance, total_income, total_expenses, transaction_count, income_count, expense_count`
+
+// Query functions //
+
+func (bs *Budgets) Index(budgetAccountID int) error {
+	pool := db.GetPool()
+	ctx := context.Background()
+	query := `SELECT id, uid, balance, total_income, total_expenses,
+						transaction_count, income_count, expense_count
+						FROM budgets WHERE budget_account_id = $1`
+
+	rows, err := pool.Query(ctx, query, budgetAccountID)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// var budgets []*Budget
+	for rows.Next() {
+		var budget Budget
+		err := rows.Scan(
+			&budget.ID,
+			&budget.UID,
+			&budget.Balance,
+			&budget.TotalIncome,
+			&budget.TotalExpenses,
+			&budget.TransactionCount,
+			&budget.IncomeCount,
+			&budget.ExpenseCount,
+		)
+		if err != nil {
+			return nil
+		}
+
+		*bs = append(*bs, budget)
+	}
+
+	return nil
 }
 
 func (b *Budget) Create(budgetAccountID int) error {
@@ -31,6 +71,27 @@ func (b *Budget) Create(budgetAccountID int) error {
 
 	b.genUID(budgetAccountID)
 	err := pool.QueryRow(ctx, query, b.UID, budgetAccountID).Scan(
+		&b.ID,
+		&b.UID,
+		&b.Balance,
+		&b.TotalIncome,
+		&b.TotalExpenses,
+		&b.TransactionCount,
+		&b.IncomeCount,
+		&b.ExpenseCount,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *Budget) FindByUID(uid string) error {
+	pool := db.GetPool()
+	ctx := context.Background()
+	query := ``
+	err := pool.QueryRow(ctx, query, uid).Scan(
 		&b.ID,
 		&b.UID,
 		&b.Balance,
