0456e444c2829331d459f68ae691fe9fef7f9c89
Ángel Díaz <ad9311@proton.me>
2024-10-14 19:37:24 -0500

    Add incomes migration

---
+++ db/migrations/20241015002554_add_incomes_table.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS incomes (
+    id SERIAL PRIMARY KEY,
+    amount NUMERIC(11,2) NOT NULL DEFAULT 0,
+    description VARCHAR(25) NOT NULL DEFAULT '',
+    budget_id INT NOT NULL,
+    entry_class_id INT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_budget
+      FOREIGN KEY (budget_id)
+      REFERENCES budgets(id)
+      ON DELETE CASCADE,
+    CONSTRAINT fk_entry_class
+      FOREIGN KEY (entry_class_id)
+      REFERENCES entry_classes(id)
+      ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS incomes
+-- +goose StatementEnd
