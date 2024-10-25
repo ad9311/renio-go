4477517ac42c3fde09539f0083fa70815a624226
Ángel Díaz <ad9311@proton.me>
2024-10-25 16:59:07 -0500

    Remove unnecessary validation package

--- internal/console/console.go
+++ internal/console/console.go
@@ -7,26 +7,24 @@ import (
 	"github.com/ad9311/renio-go/internal/envs"
 )
 
-var env = envs.GetEnvs().ENV
-
 func AppName() {
 	fmt.Print("\n--- RENIO ---\n\n")
 }
 
 func Success(message string) {
-	if env != "test" {
+	if envs.GetEnvs().ENV != "test" {
 		fmt.Printf("✓ %s\n", message)
 	}
 }
 
 func Info(message string) {
-	if env != "test" {
+	if envs.GetEnvs().ENV != "test" {
 		fmt.Printf("✓ %s\n", message)
 	}
 }
 
 func Error(message string) {
-	if env != "test" {
+	if envs.GetEnvs().ENV != "test" {
 		fmt.Printf("x %s\n", message)
 	}
 }
